c9872e66de6a2487d005984e890b8e355a484341
Per Hellström <perhells@kth.se>
2016-09-16 09:49:48 +0200

    Added padding with zeroes

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,11 +1,19 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
 function updateTime() {
     var today = new Date();
     var year = today.getFullYear();
     var month = today.getMonth();
     var day = today.getDate();
-    var hour = today.getHours();
-    var minute = today.getMinutes();
+    var hour = addZero(today.getHours());
+    var minute = addZero(today.getMinutes());
     document.getElementById('time').innerHTML = hour + ":" + minute;
     document.getElementById('date').innerHTML = day + " " + months[month] + " " + year;
     setInterval(updateTime, 1000);
